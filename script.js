196f5315003f3f3936be426bedfc921eb494fec7
Rick2702-codes <rick24656@gmail.com>
2025-01-12 17:57:55 +0530

    testing positions again

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.getElementById("main-logo").addEventListener("click", function() {
     window.location.reload(); // Refresh the page
 });
 
+alert("OffsetTop of #home: " + document.getElementById("home").offsetTop);
 // Function to calculate dynamic positions based on element offsets
 function calculateScrollTargets() {
     return {
